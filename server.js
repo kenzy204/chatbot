8e39fa7c4f000060cb3851bc1f703bee88e21f03
kenzy204 <kenzysherif204@gmail.com>
2025-09-15 01:32:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const OPENAI_KEY = process.env.OPENAI_KEY;
 
 // Chat endpoint
